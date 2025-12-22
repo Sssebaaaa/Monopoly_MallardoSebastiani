a5197701642af226c5e09231574ab2671c6899de
Ssebaaa <nicholassebastiani70@gmail.com>
2025-12-22 14:55:07 +0100

    Create Partita.java

---
+++ Files/Partita.java
@@ -0,0 +1,10 @@
+package Files;
+
+public class Partita {
+    // Componenti principali del gioco
+    private Tabellone tabellone;
+    private Giocatore[] giocatori; // Array dei partecipanti
+    private Dadi dadi;
+    private Banca banca;
+    private Mazzo mazzoImprevisti;
+    private Mazzo mazzoProbabilita;
