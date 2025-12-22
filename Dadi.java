620756132bf56031cf3282a247f12378ca932aa4
Ssebaaa <nicholassebastiani70@gmail.com>
2025-12-22 14:50:24 +0100

    Update Dadi.java

--- Dadi.java
+++ Dadi.java
@@ -7,4 +7,12 @@ public class Dadi {
         // Inizializza i dadi a 0
         valore1 = 0;
         valore2 = 0;
+    }// Lancia i dadi generando due numeri casuali da 1 a 6
+    public void lancia() {
+        // Math.random() genera un numero tra 0.0 e 0.99
+        // Moltiplicando per 6 e aggiungendo 1 otteniamo un numero da 1 a 6
+        valore1 = (int)(Math.random() * 6) + 1;
+        valore2 = (int)(Math.random() * 6) + 1;
+        
+        System.out.println("Dadi lanciati: " + valore1 + " - " + valore2);
     }
