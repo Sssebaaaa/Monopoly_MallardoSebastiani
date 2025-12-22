7a211846c3faef2d0e967b42f9ac1f89a8649d1b
Ssebaaa <nicholassebastiani70@gmail.com>
2025-12-22 14:53:10 +0100

    Update Tabellone.java

--- Files/Tabellone.java
+++ Files/Tabellone.java
@@ -5,4 +5,10 @@ public class Tabellone {
 
     public Tabellone() {
         this.caselle = new Casella[40];
+    }// Metodo per inserire una casella in una posizione specifica
+    // Utile quando la classe Partita inizializza il gioco
+    public void setCasella(int indice, Casella c) {
+        if (indice >= 0 && indice < 40) {
+            caselle[indice] = c;
+        }
     }
