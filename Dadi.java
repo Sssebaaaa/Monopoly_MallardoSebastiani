7e4a4d97fc97d5abc211bb8922881d729cee596d
Ssebaaa <nicholassebastiani70@gmail.com>
2025-12-22 14:49:42 +0100

    Update Dadi.java

---
+++ Dadi.java
@@ -0,0 +1,10 @@
+public class Dadi {
+    // Variabili per memorizzare i valori dei due dadi
+    private int valore1;
+    private int valore2;
+
+    public Dadi() {
+        // Inizializza i dadi a 0
+        valore1 = 0;
+        valore2 = 0;
+    }
