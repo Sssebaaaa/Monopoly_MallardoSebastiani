758f46fecdb835391a4266f62a92acf1cb9a5168
dxvideee <davide.mallardo08@icloud.com>
2025-12-23 17:44:31 +0100

    Creazione Casella_vaiPrigione.java

---
+++ Progetto/src/Casella_vaiPrigione.java
@@ -0,0 +1,14 @@
+package Progetto.src;
+
+class Casella_vaiPrigione extends Casella {
+
+    public Casella_vaiPrigione(String nome, int id){
+        super(nome, id);
+    }
+
+    public void azione(Giocatore g, Partita p){
+        g.setInPrigione(true);
+    }
+    
+
+}
