4f7f7ed3b68940250634ea6e52b165e1a7f40e2b
dxvideee <davide.mallardo08@icloud.com>
2025-12-23 17:46:43 +0100

    Creazione Casella_prigione.java

---
+++ Progetto/src/Casella_prigione.java
@@ -0,0 +1,16 @@
+package Progetto.src;
+
+class Casella_prigione extends Casella {
+
+    public Casella_prigione(String nome, int id){
+        super(nome, id);
+    }
+
+    public void azione(Giocatore g, Partita p) {
+        if(g.getInPrigione()){
+            System.out.println(g.getNome() + " è in prigione");
+        } else {
+            System.out.println("Solo transito");
+        }
+    }
+}
