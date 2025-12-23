46e00bc9b615828b3fd714661957d8d1501c044e
dxvideee <davide.mallardo08@icloud.com>
2025-12-23 16:54:54 +0100

    Creazione Banca.java e i suoi metodi

---
+++ Progetto/src/Banca.java
@@ -0,0 +1,35 @@
+package Progetto.src;
+
+class Banca {
+    
+    public void incassa(int somma){
+        System.out.println("Soldi incassati dalla banca: " + somma + "€");
+    }
+
+    public void vendiTerreno(Casella t, Giocatore g) {
+        int prezzo = 0;
+
+        if (t instanceof Casella_terreno) { 
+            prezzo = ((Casella_terreno) t).getValoreAcquisto();
+        } 
+        else if (t instanceof Casella_stazione) {
+            prezzo = ((Casella_stazione) t).getValoreAcquisto(); 
+        } 
+        else {
+            return; 
+        }
+
+        if (g.getSoldi() >= prezzo) {
+            g.paga(prezzo, null);   
+            g.acquistaProprieta(t); 
+
+            if (t instanceof Casella_terreno) {
+                ((Casella_terreno) t).setProprietario(g);
+            } else if (t instanceof Casella_stazione) {
+                ((Casella_stazione) t).setProprietario(g);
+            }
+        }
+    }
+
+
+}
