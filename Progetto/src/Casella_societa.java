65db91312a8d8fb0e871f3502159ae6418d25754
dxvideee <davide.mallardo08@icloud.com>
2026-01-30 22:17:34 +0100

    creazione della classe Casella_societa

---
+++ Progetto/src/Casella_societa.java
@@ -0,0 +1,51 @@
+package Progetto.src;
+
+public class Casella_societa extends Casella {
+    private int valoreAcquisto;
+    private Giocatore proprietario;
+
+    public Casella_societa(String nome, int id, int valoreAcquisto) {
+        super(nome, id);
+        this.valoreAcquisto = valoreAcquisto;
+        this.proprietario = null;
+    }
+
+    public int getValoreAcquisto() {
+        return valoreAcquisto;
+    }
+
+    public Giocatore getProprietario() {
+        return proprietario;
+    }
+
+    public void setProprietario(Giocatore proprietario) {
+        this.proprietario = proprietario;
+    }
+
+    @Override
+    public void azione(Giocatore g, Partita p) {
+        if (proprietario != null && proprietario != g) {
+            int pedaggio = calcolaAffitto(p.getDadi().getTotale());
+            g.paga(pedaggio, proprietario);
+            p.log(g.getNome() + " ha pagato " + pedaggio + "€ di pedaggio a " + proprietario.getNome());
+        } else if (proprietario == null) {
+            p.log(nome + " è in vendita per " + valoreAcquisto + "€");
+        }
+    }
+
+    public int calcolaAffitto(int sommaDadi) {
+        if (proprietario == null)
+            return 0;
+
+        int societaPossedute = 0;
+        for (Casella c : proprietario.getProprietaPossedute()) {
+            if (c instanceof Casella_societa) {
+                societaPossedute++;
+            }
+        }
+
+        // Regola standard: 4x il lancio per 1 società, 10x per entrambe
+        int moltiplicatore = (societaPossedute >= 2) ? 10 : 4;
+        return sommaDadi * moltiplicatore;
+    }
+}
